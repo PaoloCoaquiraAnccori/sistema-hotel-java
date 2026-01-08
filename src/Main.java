5d1b442f501c1cba7a42246844682fbb53fed29f
Paolo Josue Coaquira Anccori <paolojosuecoaquiraanccori@gmail.com>
2026-01-08 17:05:48 -0500

    1

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner lector = new Scanner(System.in); // asi lo en todo el codigo
+    public static Integer longitud = 100;
+
+
+    public static String linea(){
+        StringBuilder lineaRep = new StringBuilder();
+        for (int i = 0; i < longitud; i++){
+            lineaRep.append("*");
+        }
+        return lineaRep.toString();
+    }
+
+
+    public static void inicioPrograma() {
+        System.out.println(linea());
+        
+    }
+
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------------
+    public static void main(String[] args) {
+        inicioPrograma();
+    }
+}
